86316482b2915a24e3d8b98a69d0cae9c9d75fa7
Tarek-001 <205655248+Tarek-001@users.noreply.github.com>
2026-09-21 17:06:37 +0300

    solved all the exes

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
@@ -1,13 +1,22 @@
 /*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+const Firstname = "Carlos";
+const Lastname = "Stevenson";
+const Thisyear = 1965;
+const Birthyear = 1947;
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+ //The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+
 
 // Add your code right below, good luck!
+*/
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+const greeting = "Hello! My name is " + fullName + " " + "and I am " + age + " years old."
 
 
 
